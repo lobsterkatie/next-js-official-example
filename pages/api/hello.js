4146c1c750068d33965f609a23472ee000e94b0b
Katie Byers <lobsterkatie@gmail.com>
2021-11-08 11:58:18 -0800

    log NODE_ENV

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
+consloe.log({ NODE_ENV: process.env.NODE_ENV });
+
 async function handler(req, res) {
   res.status(200).json({ name: "Maisey Dog" });
 }
