079c9f95a44e00b14e4984a5205660ac0812b3d2
Katie Byers <lobsterkatie@gmail.com>
2021-11-08 12:01:12 -0800

    fix typo in hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 import * as Sentry from "@sentry/nextjs";
 
-consloe.log({ NODE_ENV: process.env.NODE_ENV });
+console.log({ NODE_ENV: process.env.NODE_ENV });
 
 async function handler(req, res) {
   res.status(200).json({ name: "Maisey Dog" });
