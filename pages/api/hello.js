a8b499a4f0a8b1ff660f70784f342a3f2274be53
Katie Byers <lobsterkatie@gmail.com>
2021-11-08 11:50:23 -0800

    use withSentry in hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,7 @@
-export default async function handler(req, res) {
+import * as Sentry from "@sentry/nextjs";
+
+async function handler(req, res) {
   res.status(200).json({ name: "Maisey Dog" });
 }
+
+export default Sentry.withSentry(handler);
