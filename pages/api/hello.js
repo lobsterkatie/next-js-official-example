10f3d35fc1df43623b35ebe10145bd68fd3290d0
Katie Byers <lobsterkatie@gmail.com>
2021-11-08 12:16:07 -0800

    use bracket syntax for setting NODE_ENV

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 import * as Sentry from "@sentry/nextjs";
 
-process.env.NODE_ENV = "dev";
+process.env["NODE_ENV"] = "dev";
 
 async function handler(req, res) {
   res.status(200).json({ name: "Maisey Dog" });
